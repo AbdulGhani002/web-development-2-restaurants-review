2c74589a0e2c7b8fb5e0c1b86df7fffb2ce3fe13
Muhammad Abdul Ghani Qureshi <itsaghani@gmail.com>
2022-12-23 10:51:27 +0500

    Error pages are created

--- app.js
+++ app.js
@@ -1,25 +1,82 @@
-const express = require("express");
-
+const fs = require("fs");
 const path = require("path");
 
-const fs = require("fs");
+const express = require("express");
+const uuid = require("uuid");
 
 const app = express();
 
-const localhost = "127.0.0.1";
-
-const port = 3000;
-
-const defaultRoutes = require("./routes/defaultRoutes");
-
-app.use(express.static("public"));
-
-app.use(express.urlencoded({ extended: false }));
-
 app.set("views", path.join(__dirname, "views"));
-
 app.set("view engine", "ejs");
 
-app.use("/", defaultRoutes);
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 
-app.listen(3000, console.log(`Listening on https://${localhost}:${port}`));
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+app.get("/restaurants", function (req, res) {
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  res.render("restaurants", {
+    numberOfRestaurants: storedRestaurants.length,
+    restaurants: storedRestaurants,
+  });
+});
+
+app.get("/restaurants/:id", function (req, res) {
+  const restaurantId = req.params.id;
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  for (const restaurant of storedRestaurants) {
+    if (restaurant.id === restaurantId) {
+      return res.render("restaurant-detail", { restaurant: restaurant });
+    }
+  }
+  res.render("404");
+});
+
+app.get("/recommend", function (req, res) {
+  res.render("recommend");
+});
+
+app.post("/recommend", function (req, res) {
+  const restaurant = req.body;
+  restaurant.id = uuid.v4();
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  storedRestaurants.push(restaurant);
+
+  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+
+  res.redirect("/confirm");
+});
+
+app.get("/confirm", function (req, res) {
+  res.render("confirm");
+});
+
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+
+app.use(function (req, res) {
+  res.render("404");
+});
+
+
+app.use(function(error,req,res,next){
+  res.render('500')
+})
+
+app.listen(3000);
