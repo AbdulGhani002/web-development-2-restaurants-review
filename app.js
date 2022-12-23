d93d00f29794f2dc908ced619b8c6f3bf765c436
Muhammad Abdul Ghani Qureshi <itsaghani@gmail.com>
2022-12-23 11:10:41 +0500

    Code is Refactored

--- app.js
+++ app.js
@@ -1,82 +1,74 @@
-const fs = require("fs");
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
-const uuid = require("uuid");
+const express = require('express');
+const uuid = require('uuid');
+
+const resData = require('./util/restaurant-data');
 
 const app = express();
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", function (req, res) {
-  res.render("index");
+app.get('/', function (req, res) {
+  res.render('index');
 });
 
-app.get("/restaurants", function (req, res) {
-  const filePath = path.join(__dirname, "data", "restaurants.json");
+app.get('/restaurants', function (req, res) {
+  const storedRestaurants = resData.getStoredRestaurants();
 
-  const fileData = fs.readFileSync(filePath);
-  const storedRestaurants = JSON.parse(fileData);
-
-  res.render("restaurants", {
+  res.render('restaurants', {
     numberOfRestaurants: storedRestaurants.length,
     restaurants: storedRestaurants,
   });
 });
 
-app.get("/restaurants/:id", function (req, res) {
+app.get('/restaurants/:id', function (req, res) {
   const restaurantId = req.params.id;
-  const filePath = path.join(__dirname, "data", "restaurants.json");
-
-  const fileData = fs.readFileSync(filePath);
-  const storedRestaurants = JSON.parse(fileData);
+  const storedRestaurants = resData.getStoredRestaurants();
 
   for (const restaurant of storedRestaurants) {
     if (restaurant.id === restaurantId) {
-      return res.render("restaurant-detail", { restaurant: restaurant });
+      return res.render('restaurant-detail', { restaurant: restaurant });
     }
   }
-  res.render("404");
+
+  res.render('404');
 });
 
-app.get("/recommend", function (req, res) {
-  res.render("recommend");
+app.get('/recommend', function (req, res) {
+  res.render('recommend');
 });
 
-app.post("/recommend", function (req, res) {
+app.post('/recommend', function (req, res) {
   const restaurant = req.body;
   restaurant.id = uuid.v4();
-  const filePath = path.join(__dirname, "data", "restaurants.json");
+  const restaurants = resData.getStoredRestaurants();
 
-  const fileData = fs.readFileSync(filePath);
-  const storedRestaurants = JSON.parse(fileData);
+  restaurants.push(restaurant);
 
-  storedRestaurants.push(restaurant);
+  resData.storeRestaurants(restaurants);
 
-  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
-
-  res.redirect("/confirm");
+  res.redirect('/confirm');
 });
 
-app.get("/confirm", function (req, res) {
-  res.render("confirm");
+app.get('/confirm', function (req, res) {
+  res.render('confirm');
 });
 
-app.get("/about", function (req, res) {
-  res.render("about");
+app.get('/about', function (req, res) {
+  res.render('about');
 });
 
 app.use(function (req, res) {
-  res.render("404");
+  res.render('404');
 });
 
-
-app.use(function(error,req,res,next){
-  res.render('500')
-})
+app.use(function (error, req, res, next) {
+  res.render('500');
+});
 
 app.listen(3000);
