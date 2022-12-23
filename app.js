6ce6de62798a91764bb7d44e23831091441c237f
Muhammad Abdul Ghani Qureshi <itsaghani@gmail.com>
2022-12-23 11:21:42 +0500

    ROutes are COnfigured

--- app.js
+++ app.js
@@ -1,74 +1,29 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const uuid = require('uuid');
-
-const resData = require('./util/restaurant-data');
+const express = require("express");
 
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+const defaultRoutes = require("./routes/default");
 
-app.use(express.static('public'));
+const restaurantRoutes = require("./routes/restaurants");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', function (req, res) {
-  res.render('index');
-});
+app.use("/", defaultRoutes);
 
-app.get('/restaurants', function (req, res) {
-  const storedRestaurants = resData.getStoredRestaurants();
-
-  res.render('restaurants', {
-    numberOfRestaurants: storedRestaurants.length,
-    restaurants: storedRestaurants,
-  });
-});
-
-app.get('/restaurants/:id', function (req, res) {
-  const restaurantId = req.params.id;
-  const storedRestaurants = resData.getStoredRestaurants();
-
-  for (const restaurant of storedRestaurants) {
-    if (restaurant.id === restaurantId) {
-      return res.render('restaurant-detail', { restaurant: restaurant });
-    }
-  }
-
-  res.render('404');
-});
-
-app.get('/recommend', function (req, res) {
-  res.render('recommend');
-});
-
-app.post('/recommend', function (req, res) {
-  const restaurant = req.body;
-  restaurant.id = uuid.v4();
-  const restaurants = resData.getStoredRestaurants();
-
-  restaurants.push(restaurant);
-
-  resData.storeRestaurants(restaurants);
-
-  res.redirect('/confirm');
-});
-
-app.get('/confirm', function (req, res) {
-  res.render('confirm');
-});
-
-app.get('/about', function (req, res) {
-  res.render('about');
-});
+app.use("/", restaurantRoutes);
 
 app.use(function (req, res) {
-  res.render('404');
+  res.render("404");
 });
 
 app.use(function (error, req, res, next) {
-  res.render('500');
+  res.render("500");
 });
 
 app.listen(3000);
